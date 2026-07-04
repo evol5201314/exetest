8feca12a68d24f0dac004fa0e4692ff05eec32ff
evol5201314 <46412120+evol5201314@users.noreply.github.com>
2026-07-04 23:42:51 +0800

    Update upload_script.py

--- root/scripts/tools/upload_script.py
+++ root/scripts/tools/upload_script.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# popup: uploadModal
+# btn: 📤 上传脚本
+# group: script
+# order: 20
+# action: runScript:upload_script.py
 beizhu = "📤 上传脚本到 /root/scripts/ (支持base64)"
 
 import os, sys, argparse
