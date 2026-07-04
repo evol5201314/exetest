9d7460b36e635db92447a6a5ff0deb8f246aac69
evol5201314 <46412120+evol5201314@users.noreply.github.com>
2026-07-04 23:41:15 +0800

    Update kill_top_process.py

--- root/scripts/tools/kill_top_process.py
+++ root/scripts/tools/kill_top_process.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# btn: 💣 清理运存
+# group: router
+# order: 70
+# action: runScript:kill_top_process.py
 beizhu = "💣 清理占用内存最高的非关键进程（自动跳过面板）"
 
 import os, sys, signal, gc, subprocess, glob, argparse
