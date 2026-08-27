b39874e93a85a8b92ece77eac9b375e0b8ebe5d6
evol5201314 <46412120+evol5201314@users.noreply.github.com>
2026-08-27 17:26:56 +0800

    Update btn_luci.py

--- root/scripts/tools/btn_luci.py
+++ root/scripts/tools/btn_luci.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 # btn: 🌐 路由器
 # group: router
 # order: 10
-# action: func:goLuci
+# action: runTool:btn_luci.py
 # btn-class: btn-dark-blue
+
+import subprocess
+
+try:
+    ip = subprocess.run(["uci", "get", "network.lan.ipaddr"], capture_output=True, text=True, timeout=2).stdout.strip()
+    if ip and '/' in ip:
+        ip = ip.split('/')[0]
+except:
+    ip = "192.168.1.2"
+
+if not ip:
+    ip = "192.168.1.2"
+
+print(f"REDIRECT:http://{ip}/cgi-bin/luci")
