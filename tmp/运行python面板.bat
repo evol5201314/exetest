48a442edf38f1ff9778e999493a2fc7952dfc1c3
evol5201314 <46412120+evol5201314@users.noreply.github.com>
2026-08-28 17:05:25 +0800

    Update 运行python面板.bat

--- tmp/运行python面板.bat
+++ tmp/运行python面板.bat
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001 >nul
 cd /d D:\tmp
-echo 面板已启动，以下为报警信息...
+echo 面板已启动，访问地址http://127.0.0.1:5000
 python D:\tmp\scripts\app.py
 echo.
 echo ============ 面板已启动，以上为报警信息 ============
